f5077d8320628acde977b2f5daa987c0192abcd2
tiarh <ihidayatullah57@gmail.com>
2022-03-09 11:24:29 +0700

    menambah variable.php

---
+++ Tugas-2/variable.php
@@ -0,0 +1,18 @@
+<?php
+    //variable
+    $a = 1;
+    $b = 3;
+   
+ 
+    echo "<br/> PENJUMLAHAN <br/>";
+    echo $a+$b; // hasilnya 4
+    
+ 
+    echo "<br/> PENGURANGAN <br/>";
+    echo ($a-$b); //hasilnya -2
+    
+ 
+    echo "<br/> PERKALIAN <br/>";
+    echo ($a*$b); //hasilnya  3
+    
+?>
